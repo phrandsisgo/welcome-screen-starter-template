204ce2a282cf568da5f3056f307987e19c40ec10
Francisco Wohlgemuth <francisco.wohlgemuth@hotmail.com>
2023-09-07 11:58:32 +0200

    done basic frontend work

---
+++ src/Card.vue
@@ -0,0 +1,17 @@
+<script>
+export default{
+    props:{
+
+    }
+}
+</script>
+<template>
+<div class="card">
+      <p class="timeStamp">{{timestamp}}</p>
+      <p class="cardTitle">  {{ cardtitle }}</p>
+      <p class="cardBeschreibung"> {{cardDescription}}</p>
+
+    </div>
+</template>
+<style>
+</style>
