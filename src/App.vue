0d70849df2f8a11d6524e68d744ecf2dfd1e427e
Francisco Wohlgemuth <francisco.wohlgemuth@hotmail.com>
2023-09-07 15:58:36 +0200

    Kurz vor dem Array

--- src/App.vue
+++ src/App.vue
@@ -6,42 +6,111 @@ export default {
       title: "Welcome to Opportunity",
       sheet_id: import.meta.env.VITE_GOOGLE_SHEET_ID,
       api_token: import.meta.env.VITE_GOOGLE_API_KEY,
+     //api_token: "AIzaSyBSzduu2jm5uF3pCFYrv98YIrvFgSd5kXY",
+     entries:[],
+      cardItem : [
+        {
+          id: 1,
+          date: '07.09.2023 , 14:00',
+          title: 'Druckerpapier einkaufen',
+          description: 'Da uns kürzlich die Druckerpatronen ausgegangen sind, müssen für die HR-Abteilung neue Druckerpatronen gekauft werden.',
+        },
+        {
+          id: 2,
+          date: '08.09.2023 , 10:30',
+          title: 'Besprechung mit dem Vertriebsteam',
+          description: 'Besprechung mit dem Vertriebsteam, um die Verkaufsstrategie für das kommende Quartal zu planen.',
+        },
+        {
+          id: 3,
+          date: '09.09.2023 , 16:15',
+          title: 'Monatlicher Finanzbericht erstellen',
+          description: 'Erstellung des monatlichen Finanzberichts für die Geschäftsführung und Analyse der finanziellen Leistung des Unternehmens.',
+        },
+        {
+          id: 4,
+          date: '10.09.2023 , 13:45',
+          title: 'Kundengespräch mit XYZ Inc.',
+          description: 'Telefonisches Gespräch mit dem Kunden XYZ Inc. bezüglich ihres laufenden Projekts und Klärung von offenen Fragen.',
+        },
+        {
+          id: 5,
+          date: '11.09.2023 , 09:00',
+
+          title: 'Mitarbeitergespräche führen',
+          description: 'Durchführung von jährlichen Mitarbeitergesprächen und Festlegung von Zielen für das kommende Jahr.',
+        },
+        {
+          id: 6,
+
+          date: '12.09.2023 , 15:30',
+          title: 'Besprechung mit dem IT-Team',
+          description: 'Besprechung mit dem IT-Team, um technische Anforderungen für das neue Projekt zu erörtern und Ressourcen zuzuweisen.',
+        },
+      ],
+    
     };
   },
+  computed: {
+    gsheet_url() {
+      return `https://sheets.googleapis.com/v4/spreadsheets/${this.sheet_id}/values:batchGet?ranges=A1%3AE100&valueRenderOption=FORMATTED_VALUE&key=${this.api_token}`;
+    }
+  },
+  mounted() {
+    this.getData(); // get first initial data and then wait for the next update
+  },
+  methods: {
+      async getData() {
+        const response = await fetch(this.gsheet_url);
+        const data = await response.json();
+        this.entries = data.valueRanges[0].values;
+        //console.log("this is the data" +JSON.stringify(data));
+        console.log("this is the data" +this.entries);
+      }
+  },
+
+
 };
 
 const cardItem = [
   {
+    id: 1,
     date: '07.09.2023 , 14:00',
     title: 'Druckerpapier einkaufen',
     description: 'Da uns kürzlich die Druckerpatronen ausgegangen sind, müssen für die HR-Abteilung neue Druckerpatronen gekauft werden.',
   },
   {
+    id: 2,
     date: '08.09.2023 , 10:30',
     title: 'Besprechung mit dem Vertriebsteam',
     description: 'Besprechung mit dem Vertriebsteam, um die Verkaufsstrategie für das kommende Quartal zu planen.',
   },
   {
+    id: 3,
     date: '09.09.2023 , 16:15',
     title: 'Monatlicher Finanzbericht erstellen',
     description: 'Erstellung des monatlichen Finanzberichts für die Geschäftsführung und Analyse der finanziellen Leistung des Unternehmens.',
   },
   {
+    id: 4,
     date: '10.09.2023 , 13:45',
     title: 'Kundengespräch mit XYZ Inc.',
     description: 'Telefonisches Gespräch mit dem Kunden XYZ Inc. bezüglich ihres laufenden Projekts und Klärung von offenen Fragen.',
   },
   {
+    id: 5,
     date: '11.09.2023 , 09:00',
     title: 'Mitarbeitergespräche führen',
     description: 'Durchführung von jährlichen Mitarbeitergesprächen und Festlegung von Zielen für das kommende Jahr.',
   },
   {
+    id: 6,
     date: '12.09.2023 , 15:30',
     title: 'Besprechung mit dem IT-Team',
     description: 'Besprechung mit dem IT-Team, um technische Anforderungen für das neue Projekt zu erörtern und Ressourcen zuzuweisen.',
   },
 ];
+
 </script>
 <template>
   <div id="application">
@@ -49,15 +118,23 @@ const cardItem = [
     <h3 class="site-date">{{new Date().toLocaleDateString('en-GB').replace(/\//g, '.')}}</h3>
     <div class="card">
       <p class="timeStamp">14.00 Uhr</p>
-      <p class="cardTitle"> Basisbeschäftigung Besuch</p>
+      <p class="cardTitle">Projekt fertigstellen</p>
       <p class="cardBeschreibung"> Interesse für den zweiten Kurs werden uns besuchen</p>
+    </div>
+
+   
+
+    <div class="card" v-for="card in cardItem">
+      {{ card.id }}
+      <p class="timeStamp">{{card.date}}</p>
+      <p class="cardTitle"> {{ card.title }}</p>
+      <p class="cardBeschreibung"> {{ card.description }}</p>
 
     </div>
-    <Card>
-
-
-
-    </Card>
+    <div id="datenEinsicht" v-for="eintrag in entries">
+      Hallo
+    </div>
+    <br><br><br>
   </div>
   <footer>
     <div id="footerLogos">
@@ -79,6 +156,7 @@ const cardItem = [
 .card{
   background-color: darkblue;
   padding: 10px;
+  margin-top: 15px;
 }
 .site-date{
   color: grey;
